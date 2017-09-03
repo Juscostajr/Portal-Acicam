95aaac1f9f560e84b1a98676efa27190be2f5b94
Juscostajr <juscostajr@gmail.com>
2017-09-03 18:14:21 -0300

    mensagens de informação e alerta

--- admin/new_noticia.php
+++ admin/new_noticia.php
@@ -8,16 +8,25 @@ $srcFoto = date("YmdHis");
 
 <br>
 
-<form method='post' action='' enctype='multipart/form-data' >
-    <div class='form-group'>  <input class='form-control' id="titulo" type='text' name='titulo' maxlength="60" placeholder='Digite aqui o título da notícia' required>
+<form method='post' action='' enctype='multipart/form-data'>
+    <div class='form-group'>
+        <label>Título</label>
+        <input class='form-control' id="titulo" type='text' name='titulo' maxlength="60"
+                                   placeholder='Digite aqui o título da notícia' required>
         <div id="contador" class="text-right"></div>
     </div>
-    <div class='form-group'><input id='file' type='file' name='foto' class='file' data-show-preview='true'></div>
+    <div class='form-group'>
+        <label>Imagem</label>
+        <input id='file' type='file' name='foto' class='file' data-show-preview='true'></div>
     <input type='hidden' name='imag' value='<?= $srcFoto ?>'>
+    <div class="alert alert-info">
+        <span class="glyphicon glyphicon-info-sign"></span> Se você utiliza o Google Chrome, utilize CTRL+SHIFT+V para colar sem formatação.
+    </div>
     <div class='form-group'>
         <div id="summernote"><p></p></div>
         <textarea name='texto' id='hospedeira' hidden></textarea></div>
-    <div class='form-group'><input type='submit' name='submit' class='btn btn-success btn-block' id='submit-btn'></form></div>
+    <div class='form-group'><input type='submit' name='submit' class='btn btn-success btn-block' id='submit-btn'>
+</form></div>
 </form>
 
 <script type="text/javascript" src="js/noticia.js"></script>
--- admin/new_pagina.php
+++ admin/new_pagina.php
@@ -9,14 +9,26 @@ $srcFoto = date("YmdHis");
 <br>
 
 <form method='post' action='' enctype='multipart/form-data' >
-    <div class='form-group'>  <input class='form-control' id="url" type='text' name='url' maxlength="60" placeholder='Digite aqui link da página (ex: meu-link)' required>
+    <div class='form-group'>
+        <div class="alert alert-warning">
+            <strong>Atenção!</strong> Evite colocar espaços ou quaisquer tipos de caracteres especiais ou latinos no link da página
+        </div>
+        <label>Link</label>
+        <input class='form-control' id="url" type='text' name='url' maxlength="60" placeholder='Digite aqui link da página (ex: meu-link)' required>
         <div id="contador" class="text-right"></div>
     </div>
-    <div class='form-group'>  <input class='form-control' id="titulo" type='text' name='titulo' maxlength="60" placeholder='Digite aqui o título da notícia' required>
+    <div class='form-group'>
+        <label>Título</label>
+        <input class='form-control' id="titulo" type='text' name='titulo' maxlength="60" placeholder='Digite aqui o título da notícia' required>
         <div id="contador" class="text-right"></div>
     </div>
-    <div class='form-group'>  <input class='form-control' id="keywords" type='text' name='keywords' maxlength="60" placeholder='Digite aqui os keywords' required>
+    <div class='form-group'>
+        <label>Palavras Chave</label>
+        <input class='form-control' id="keywords" type='text' name='keywords' maxlength="60" placeholder='Digite aqui os keywords' required>
         <div id="contador" class="text-right"></div>
+    </div>
+    <div class="alert alert-info">
+        <span class="glyphicon glyphicon-info-sign"></span> Se você utiliza o Google Chrome, utilize CTRL+SHIFT+V para colar sem formatação.
     </div>
     <div class='form-group'>
         <div id="summernote"><p></p></div>
