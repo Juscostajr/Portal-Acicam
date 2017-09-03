4133c4bd9d9273c6a8d18fe6cb63974d0d159ebc
Juscostajr <juscostajr@gmail.com>
2017-09-03 18:12:37 -0300

    Implementado copy to clipboard

--- admin/edt_midia.php
+++ admin/edt_midia.php
@@ -16,8 +16,14 @@ $patch = '../assets/midia/';
 $files = array_diff(scandir($patch), array('.', '..'));
 ?>
 <div class="row">
-    <div class="col-md-10"><div class='form-group'><input id='file' type='file' name='foto' class='file' data-show-preview='true'></div></div>
-    <div class="col-md-2"><button type="button" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-upload"></span> Carregar</button></div>
+    <div class="col-md-10">
+        <div class='form-group'><input id='file' type='file' name='foto' class='file' data-show-preview='true'></div>
+    </div>
+    <div class="col-md-2">
+        <button type="button" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-upload"></span>
+            Carregar
+        </button>
+    </div>
 </div>
 <div class="table-responsive">
     <table class="table table-striped">
@@ -26,12 +32,13 @@ $files = array_diff(scandir($patch), array('.', '..'));
             <th>Nome do Arquivo</th>
             <th>Tamanho</th>
             <th>Data de Modificação</th>
+            <th>Link</th>
             <th>Ação</th>
         </tr>
         </thead>
         <tbody>
         <?php foreach ($files as $key => $value) : ?>
-            <?php $file = new SplFileInfo($patch.$value) ?>
+            <?php $file = new SplFileInfo($patch . $value) ?>
             <tr>
                 <td>
                     <span class="document-item" filetype="<?= $file->getExtension() ?>">
@@ -40,14 +47,25 @@ $files = array_diff(scandir($patch), array('.', '..'));
                     </span>
                 </td>
                 <td>
-                    <?= human_filesize($file->getSize())?>
+                    <?= human_filesize($file->getSize()) ?>
                 </td>
-                <td><?= date ("d/m/Y H:i:s", $file->getCTime()) ?></td>
+                <td><?= date("d/m/Y H:i:s", $file->getCTime()) ?></td>
                 <td>
                     <form method='post'>
-                        <input type='hidden' name='file' value='<?= $file->getFilename() ?>'>
-                        <button type='button' class='btn btn-danger btn-xs' onClick='this.parentNode.submit();'><span class='glyphicon glyphicon-trash'></span></button>
+                        <div class="input-group">
+                            <input type='text' name='file' value='/assets/midia/<?= urlencode($file->getFilename()) ?>'
+                                   class="form-control">
+                            <span class="input-group-btn">
+                            <button class="btn btn-warning clipboard" type="button">
+                                <span class="glyphicon glyphicon-copy"></span>
+                            </button>
+                            </span>
+                        </div>
                     </form>
+                </td>
+                <td>
+                    <button type='button' class='btn btn-danger' onClick='this.parentNode.submit();'><span
+                            class='glyphicon glyphicon-trash'></span></button>
                 </td>
             </tr>
         <?php endforeach ?>
@@ -55,4 +73,27 @@ $files = array_diff(scandir($patch), array('.', '..'));
     </table>
 
 </div>
-<script type="text/javascript" src="js/noticia.js"></script>
+<script type="text/javascript" src="js/clipboard.min.js"></script>
+<script type="text/javascript">
+
+
+    var clipboard = new Clipboard('.clipboard', {
+        target: function (trigger) {
+            return trigger.parentNode.previousElementSibling;
+        }
+    });
+
+    clipboard.on('success', function (e) {
+        console.log(e);
+    });
+
+    clipboard.on('error', function (e) {
+        console.log(e);
+    });
+
+    $("#file").fileinput({
+        language: "pt-BR",
+        showUpload: false,
+        allowedFileExtensions: ["jpg", "png", "gif"]
+    });
+</script>
